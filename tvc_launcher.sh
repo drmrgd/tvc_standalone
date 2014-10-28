6b52aa69daa4b09a90595b6db69e2652b25808c4
ionadmin <ionadmin@mocha-ir.ncifcrf.gov>
2014-10-28 09:11:56 -0400

    Added CLI Opts and the ability for a custom config file

--- tvc_launcher.sh
+++ tvc_launcher.sh
@@ -5,27 +5,80 @@
 #
 # 7/2/2014 - D Sims
 ###################################################################################################################
-version="v0.5.1_070314"
+version="v0.7.0_102814"
 script=$(basename $0)
 debug=1
-cwd=$(pwd)
-now() { now=$(date +%c); echo -n "[$now]:"; }
+usage="$( cat <<EOT
+$script [options] <output_root_dir> <bam_file>
+
+Launcher script for standalone version of TVC created to make adjusting parameters and batch running a little
+more simple.  
+
+Script will read a default config file located in the current working directory by default.  If a custom config 
+is desired, one can be fed to the script using the '-c' option.  The file must out line the following parameters:
+
+    TVC_ROOT_DIR        : Location of the TVC binaries
+    REFERENCE           : Location of the hg19.fasta reference sequence
+    TVC_PARAM           : Location of the TVC paramters JSON file to be use for the analysis
+    BED_MERGED_PLAIN    : Location of Plain, Merged regions BED file.
+    BED_UNMERGED_DETAIL : Location of the Detailed, Unmerged regions BED file.
+    HOTSPOT_VCF         : Location of the hotspots VCF file to be used in the analysis
+
+For the time being, all of these parameters must be used in the config file.  For an example, see the default config
+file in the current directory
+
+Other options:
+    -c    Use a custom config file instead of the default in the current working dir
+    -v    Print the version information and exit.
+    -h    Print this help text
+EOT
+)"
 
 # Set up logging for run to capture std TVC output to data dir start log in cwd and then move to results later
 logfile="$(date +"%Y%m%d").tvc_sa.log.txt"
 exec > >(tee $logfile)
 exec 2>&1
 
+while getopts :c:hv opt;
+do 
+    case $opt in
+        c)
+            config_file="$OPTARG"
+            if [[ ! -e "$config_file" ]]; then
+                echo "ERROR: Selected config file '$config_file' can not be found"
+                exit 0
+            fi
+            ;;
+        v)
+            echo -e "$script - $version\n"
+            exit 0
+            ;;
+        h)
+            echo "$script - $version"
+            echo -e "$usage\n"
+            exit 0
+            ;;
+        /?)
+            echo "Invalid option: -$OPTARG\n"
+            echo -e "$usage\n"
+            exit 1
+            ;;
+    esac
+done
+shift $((OPTIND - 1))
+
 # Check that we've loaded an output dir and a bam file to process.
 if (( $# < 2 )); then
     echo "ERROR: Invalid number of arguments!"
-    echo
-    echo "USAGE: $script [options] <output_root_dir> <bam_file>"
+    echo -e "$usage\n"
     exit 1
 fi
 
 outdir_root=$1
 input_bam=$2
+
+cwd=$(pwd)
+now() { now=$(date +%c); echo -n "[$now]:"; }
 
 # Get the sample name assuming <sample_name>_<rawlib>?.<bam>
 sample_name=$(echo $(basename $input_bam) | perl -pe 's/(.*?)(_rawlib)?.bam/\1/')
@@ -62,9 +115,10 @@ if [[ -d "$outdir" ]]; then
         mkdir -p $outdir
 fi
 
-
-# Read in a config file and get input params
-config_file="$cwd/config"
+# If no custom config file set, read in the default in the cwd 
+if [[ -z $config_file ]]; then
+    config_file="$cwd/config"
+fi
 declare -A tvc_params
 
 if [[ ! -e "$config_file" ]]; then 
@@ -72,6 +126,7 @@ if [[ ! -e "$config_file" ]]; then
     exit 1;
 fi
 
+err_count=0
 while read key value;
 do
     if [[ ! $key =~ ^\ *# && -n $key ]]; then
@@ -79,9 +134,20 @@ do
             tvc_params[$key]=$value
         else
             tvc_params[$key]="NULL"
+            err_count=1
         fi
     fi
 done < $config_file
+
+if [[ $err_count == 1 ]]; then
+    echo "ERROR: There were invalid parameters found in the config file.  Can not continue unless the following params are fixed:"
+    for err in "${!tvc_params[@]}"; do
+        if [[ ${tvc_params[$err]} == 'NULL' ]]; then
+            printf "\t%-20s  => %s\n" $err ${tvc_params[$err]}
+        fi
+    done
+    exit 1
+fi
 
 # Need to set up a TVC Root so that the rest of the scripts can be found
 export TVC_BIN_DIR=${tvc_params[TVC_ROOT_DIR]}
